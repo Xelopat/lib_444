6c5a8643b47328bf2202f5aa24917dabc0fc45b0
Xelopat <61018159+Xelopat@users.noreply.github.com>
2021-05-24 05:45:24 +0300

    Add files via upload

--- all_books.php
+++ all_books.php
@@ -9,36 +9,36 @@
 		<link rel="stylesheet" href="style.css">
 		<title>Все книги</title>
 	</head>
-	<body>
+	<body><br/>
 		<div>
 			<div style="float:left;">
-				<a href="index.php">Назад</a>
+				<a class=button href="index.php">Назад</a>
 			</div>
 			<div style="float:right;">
 				<?php
 					
 					if($_SESSION["my_login"] == "") echo 
 					'<div>
-					<a href="login.php">Вход</a>
-					<a href="registration.php">Регистрация</a>
+					<a class=button href="login.php">Вход</a>
+					<a class=button href="registration.php">Регистрация</a>
 					</div>';
 					else echo 
 					'<div>
-					<a href="profile.php">' . $_SESSION["my_login"] . '</a>
-					<a href="logout.php">Выход</a>
+					<a class=button href="profile.php">' . $_SESSION["my_login"] . '</a>
+					<a class=button href="logout.php">Выход</a>
 					</div>';
 				?>
 			</div>
 			<div class="clear"></div>
 		</div>
 		<div align="center">
-			<label>Класс:</label>
-			<select id="class_num" onchange=reload('')>
+			<label style="font-size: 30px">Класс:</label>
+			<select id="class_num" onchange=reload('') style="font-size: 20px">
 				<option value="0">Не выбрано</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option>
 				<option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option>
 			</select>
-			<label>Автор:</label>
-			<select id="author_id" onchange=reload('')>
+			<label style="font-size: 30px">Автор:</label>
+			<select id="author_id" onchange=reload('') style="font-size: 30px">
 				<option value="0">Не выбрано</option>
 				<?php
 					$all_authors = all_authors();
@@ -47,8 +47,8 @@
 					}
 				?>
 			</select>
-			<label>Предмет:</label>
-			<select id="subject_id" onchange=reload('')>
+			<label style="font-size: 30px">Предмет:</label>
+			<select id="subject_id" onchange=reload('') style="font-size: 30px">
 				<option value="0">Не выбрано</option>
 				<?php
 					$all_subjects = all_subjects();
@@ -58,7 +58,7 @@
 				?>
 			</select>
 		</div></br>
-		<table border="1" align="center">
+		<table border="1" align="center" width=50%>
 			<?php
 				$args = array($_GET["author_id"], $_GET["subject_id"], $_GET["class_num"]);
 				$info = "";
@@ -77,31 +77,31 @@
 				$info .= implode(" AND ", $all_info);
 				if($_GET["order"] != "") $info .= " ORDER BY " . $_GET["order"];
 				if(!$status){
-					echo "<td><p onclick=reload('author_id')>Автор</p></td><td><p onclick=reload('subject_id')>Предмет</p></td><td><p onclick=reload('class_num')>Класс</p></td>";
+					echo "<tr height=40px><td style='font-size:30px;'><p onclick=reload('author_id')>Автор</p></td><td style='font-size:30px;'><p onclick=reload('subject_id')>Предмет</p></td><td style='font-size:30px;'><p onclick=reload('class_num')>Класс</p></td></tr>";
 					$all_books = all_books($info);
 					if($all_books){
 						foreach($all_books as $row){
-							echo "<tr>";
+							echo "<tr height=40px>";
 							$author = get_author($row["author_id"]);
 							$subject = get_subject($row["subject_id"]);
 							$class = $row["class_num"];
-							echo "<td>$author</td><td>$subject</td><td>$class</td>";
+							echo "<td style='font-size:30px;'>$author</td><td style='font-size:30px;'>$subject</td><td style='font-size:30px;'>$class</td>";
 							echo "</tr>";
 						}
 					}
 				}
 				else{
-					echo "<td><p onclick=reload('author_id')>Автор</p></td><td><p onclick=reload('subject_id')>Предмет</p></td><td><p onclick=reload('class_num')>Класс</p></td><td><p>Добавить в библиотеку</p></td>";
+					echo "<tr height=40px><td style='font-size:30px;'><p onclick=reload('author_id')>Автор</p></td><td style='font-size:30px;'><p onclick=reload('subject_id')>Предмет</p></td><td style='font-size:30px;'><p onclick=reload('class_num')>Класс</p></td><td style='font-size:30px;'><p>Добавить в библиотеку</p></td></tr>";
 					$all_books = all_books($info);
 					if($all_books){
 						foreach($all_books as $row){
-							echo "<tr>";
+							echo "<tr height=40px>";
 							$author = get_author($row["author_id"]);
 							$subject = get_subject($row["subject_id"]);
 							$class = $row["class_num"];
 							$arr = $row['id'] . '_take';
-							if(!i_have_book($_SESSION["my_id"], $row["id"])) echo "<td>$author</td><td>$subject</td><td>$class</td><td align='center'><button onclick=send_form('$arr');>&#9989;</button></td>";
-							else echo "<td>$author</td><td>$subject</td><td>$class</td><td align='center'>Уже в библиотеке</td>";
+							if(!i_have_book($_SESSION["my_id"], $row["id"])) echo "<td style='font-size:30px;'>$author</td><td style='font-size:30px;'>$subject</td><td style='font-size:30px;'>$class</td><td style='font-size:30px;' align='center'><p style='height: 40px; width: 40px; font-size:30px;cursor:pointer' onclick=send_form('$arr',this);>&#9989;</p></td>";
+							else echo "<td style='font-size:30px;'>$author</td><td style='font-size:30px;'>$subject</td><td style='font-size:30px;'>$class</td><td style='font-size:20px;' align='center'>Уже в библиотеке</td>";
 							echo "</tr>";
 						}
 					}
@@ -111,40 +111,40 @@
 	</body>
 	<script>
 		const searchString = new URLSearchParams(window.location.search);
-	function reload(sort_by){
-	let class_num = document.getElementById("class_num").value;
-	let author_id = document.getElementById("author_id").value;
-	let subject_id = document.getElementById("subject_id").value;
-	let info = "?";
-	if(class_num != "0") info += "class_num=" + class_num + "&";
-	if(author_id != "0") info += "author_id=" + author_id + "&";
-	if(subject_id != "0") info += "subject_id=" + subject_id + "&";
-	if(searchString.get('order') != "" && sort_by == "") sort_by = searchString.get('order');
-	if(sort_by != "") info += "order=" + sort_by + "&";
-	document.location.href = "all_books.php" + info;
-	}
-	if(searchString.get('class_num')) document.getElementById("class_num").value = searchString.get('class_num');
-	if(searchString.get('author_id')) document.getElementById("author_id").value = searchString.get('author_id');
-	if(searchString.get('subject_id')) document.getElementById("subject_id").value = searchString.get('subject_id');
-	
-	async function send_form(arr){
-	let formData = new FormData();
-	let id = arr.split("_")[0];
-	let type = arr.split("_")[1];
-	formData.append('id', id);
-	formData.append('type', type);
-	let response = await fetch('write_db.php', {
-	method: 'POST',
-	body: formData
-	});
-	let result = await response.text();
-	if(result == "failed") alert("У вас уже есть эта книга"); 
-	else location.reload();
-	}
-	</script>
-	</html>
-	
-	
-	
-	
+		function reload(sort_by){
+			let class_num = document.getElementById("class_num").value;
+			let author_id = document.getElementById("author_id").value;
+			let subject_id = document.getElementById("subject_id").value;
+			let info = "?";
+			if(class_num != "0") info += "class_num=" + class_num + "&";
+			if(author_id != "0") info += "author_id=" + author_id + "&";
+			if(subject_id != "0") info += "subject_id=" + subject_id + "&";
+			if(searchString.get('order') != "" && sort_by == "") sort_by = searchString.get('order');
+			if(sort_by != "") info += "order=" + sort_by + "&";
+			document.location.href = "all_books.php" + info;
+		}
+		if(searchString.get('class_num')) document.getElementById("class_num").value = searchString.get('class_num');
+		if(searchString.get('author_id')) document.getElementById("author_id").value = searchString.get('author_id');
+		if(searchString.get('subject_id')) document.getElementById("subject_id").value = searchString.get('subject_id');
 		
+		async function send_form(arr, button){
+			button.innerHTML = "Уже в библиотеке";
+			button.style = 'font-size:20px;'
+			let formData = new FormData();
+			let id = arr.split("_")[0];
+			let type = arr.split("_")[1];
+			formData.append('id', id);
+			formData.append('type', type);
+			let response = await fetch('write_db.php', {
+				method: 'POST',
+				body: formData
+			});
+			let result = await response.text();
+			//else location.reload();
+		}
+	</script>
+</html>
+
+
+
+
