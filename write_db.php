a1a77f54445c9ee4c60f861461a3e92626511a81
Xelopat <61018159+Xelopat@users.noreply.github.com>
2021-05-24 05:42:07 +0300

    Add files via upload

--- write_db.php
+++ write_db.php
@@ -5,20 +5,23 @@
 	if($_POST["type"] == "remove"){
 		remove_book($_SESSION["my_id"], (int)$_POST["id"], "taked_books");
 		append_book($_SESSION["my_id"], (int)$_POST["id"], "waited_books");
+		$_SESSION["take_count"] -= 1;
+		reload_count($_POST["user_id"], "take_count", -1);
+		$_SESSION["wait_count"] += 1;
+		reload_count($_POST["user_id"], "wait_count", 1);
 	}
 	else if($_POST["type"] == "take"){
-		if(!i_have_book($_SESSION["my_id"], $_POST["id"])){
-			append_book($_SESSION["my_id"], (int)$_POST["id"], "taked_books");
-			echo "comlete";
-		}
-		else echo "failed";
+		append_book($_SESSION["my_id"], (int)$_POST["id"], "taked_books");
+		$_SESSION["take_count"] += 1;
+		reload_count($_SESSION["my_id"], "take_count", 1);
+		echo "comlete";
 	}
 	else if($_POST["type"] == "return"){
 		remove_book($_SESSION["my_id"], (int)$_POST["id"], "waited_books");
 		append_book($_SESSION["my_id"], (int)$_POST["id"], "taked_books");
-	}
-	else if($_POST["type"] == "accept"){
-		remove_book($_SESSION["my_id"], (int)$_POST["id"], "waited_books");
-		append_book($_SESSION["my_id"], (int)$_POST["id"], "accepted_books");
+		$_SESSION["take_count"] += 1;
+		reload_count($_SESSION["my_id"], "take_count", 1);
+		$_SESSION["wait_count"] -= 1;
+		reload_count($_SESSION["my_id"], "wait_count", -1);
 	}
 ?>
