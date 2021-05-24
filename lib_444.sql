ff9d5ea1cde87f1fa675b4d7644004d36b6e37af
Xelopat <61018159+Xelopat@users.noreply.github.com>
2021-05-24 05:46:58 +0300

    Add files via upload

--- lib_444.sql
+++ lib_444.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Май 13 2021 г., 19:59
+-- Время создания: Май 24 2021 г., 06:46
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -32,27 +32,7 @@ CREATE TABLE IF NOT EXISTS `accepted_books` (
   `id_book` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
-
---
--- Дамп данных таблицы `accepted_books`
---
-
-INSERT INTO `accepted_books` (`id`, `id_user`, `id_book`, `date`) VALUES
-(1, 1, 1, '2021-05-06 19:19:08'),
-(2, 1, 6, '2021-05-06 21:43:38'),
-(3, 1, 5, '2021-05-06 21:43:38'),
-(4, 1, 7, '2021-05-06 21:43:38'),
-(5, 1, 4, '2021-05-06 21:43:38'),
-(6, 1, 1, '2021-05-12 19:22:27'),
-(7, 1, 0, '2021-05-12 19:30:17'),
-(8, 1, 1, '2021-05-12 19:33:28'),
-(9, 1, 5, '2021-05-12 19:35:24'),
-(10, 1, 4, '2021-05-12 19:39:20'),
-(11, 1, 6, '2021-05-12 19:40:16'),
-(12, 1, 7, '2021-05-12 19:43:52'),
-(13, 1, 6, '2021-05-12 19:44:22'),
-(14, 1, 1, '2021-05-12 19:44:46');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
 
 -- --------------------------------------------------------
 
@@ -64,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `authors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
 -- Дамп данных таблицы `authors`
@@ -76,7 +56,11 @@ INSERT INTO `authors` (`id`, `name`) VALUES
 (3, 'Ульянов.Р.К'),
 (4, 'Панфилов.В.Д'),
 (5, 'Соловьёв.Р.Д'),
-(6, 'Варшавсий.А.И');
+(6, 'Варшавсий.А.И'),
+(7, 'Иванов К.С.'),
+(8, 'Звонилин Р.Т.'),
+(9, 'Севернов А.Б.'),
+(10, 'Светлов.В.Р.');
 
 -- --------------------------------------------------------
 
@@ -89,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `books` (
   `author_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
   `class_num` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `info` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
@@ -103,23 +87,34 @@ CREATE TABLE IF NOT EXISTS `books` (
   UNIQUE KEY `id_10` (`id`),
   KEY `id_2` (`id`),
   KEY `id_11` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
 
 --
 -- Дамп данных таблицы `books`
 --
 
 INSERT INTO `books` (`id`, `author_id`, `subject_id`, `class_num`, `date`, `info`) VALUES
-(1, 1, 1, 1, '2021-05-06', 'Книга'),
-(4, 4, 3, 8, '0000-00-00', 'Книга_1'),
-(5, 2, 1, 8, '0000-00-00', 'Книга_2'),
-(6, 5, 2, 10, '0000-00-00', 'Книга_3'),
-(7, 1, 2, 4, '0000-00-00', 'Вата'),
-(8, 1, 1, 5, '0000-00-00', ''),
-(9, 1, 2, 3, '0000-00-00', ''),
-(10, 1, 5, 3, '0000-00-00', ''),
-(11, 5, 4, 3, '0000-00-00', ''),
-(12, 2, 4, 3, '0000-00-00', '');
+(13, 1, 10, 6, '0000-00-00 00:00:00', ''),
+(14, 1, 7, 6, '0000-00-00 00:00:00', ''),
+(15, 2, 9, 7, '0000-00-00 00:00:00', ''),
+(16, 4, 9, 7, '0000-00-00 00:00:00', ''),
+(17, 3, 8, 8, '0000-00-00 00:00:00', ''),
+(18, 3, 6, 8, '0000-00-00 00:00:00', ''),
+(19, 4, 7, 9, '0000-00-00 00:00:00', ''),
+(20, 4, 8, 9, '0000-00-00 00:00:00', ''),
+(21, 5, 6, 10, '0000-00-00 00:00:00', ''),
+(22, 5, 2, 10, '0000-00-00 00:00:00', ''),
+(23, 6, 5, 11, '0000-00-00 00:00:00', ''),
+(24, 6, 7, 11, '0000-00-00 00:00:00', ''),
+(25, 7, 4, 4, '0000-00-00 00:00:00', ''),
+(26, 7, 9, 10, '0000-00-00 00:00:00', ''),
+(27, 8, 3, 4, '0000-00-00 00:00:00', ''),
+(28, 8, 6, 7, '0000-00-00 00:00:00', ''),
+(29, 9, 2, 7, '0000-00-00 00:00:00', ''),
+(30, 9, 4, 1, '0000-00-00 00:00:00', ''),
+(31, 10, 1, 1, '0000-00-00 00:00:00', ''),
+(32, 10, 7, 4, '0000-00-00 00:00:00', ''),
+(33, 4, 3, 7, '2021-05-24 01:58:05', '');
 
 -- --------------------------------------------------------
 
@@ -132,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `classes` (
   `name` varchar(7) NOT NULL,
   `num` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
 
 --
 -- Дамп данных таблицы `classes`
@@ -145,12 +140,19 @@ INSERT INTO `classes` (`id`, `name`, `num`) VALUES
 (4, '2А', 2),
 (5, '2Б', 2),
 (6, '2В', 2),
-(7, '3А', 3),
 (8, '3Б', 3),
 (9, '3В', 3),
-(10, '4А', 4),
+(10, '7В', 7),
 (11, '4Б', 4),
-(12, '4В', 4);
+(12, '4В', 4),
+(13, '5А', 5),
+(14, '5Б', 5),
+(15, '5В', 5),
+(16, '6А', 6),
+(17, '6Б', 6),
+(18, '6В', 6),
+(19, '7А', 7),
+(20, '7Б', 7);
 
 -- --------------------------------------------------------
 
@@ -162,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `subjects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
 
 --
 -- Дамп данных таблицы `subjects`
@@ -174,7 +176,11 @@ INSERT INTO `subjects` (`id`, `name`) VALUES
 (3, 'Информатика'),
 (4, 'Литература'),
 (5, 'Русский'),
-(6, 'Геометрия');
+(6, 'Геометрия'),
+(7, 'История'),
+(8, 'Черчение'),
+(9, 'Биология'),
+(10, 'Английский');
 
 -- --------------------------------------------------------
 
@@ -188,18 +194,7 @@ CREATE TABLE IF NOT EXISTS `taked_books` (
   `id_book` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=58 ;
-
---
--- Дамп данных таблицы `taked_books`
---
-
-INSERT INTO `taked_books` (`id`, `id_user`, `id_book`, `date`) VALUES
-(53, 0, 6, '2021-05-07 19:58:46'),
-(54, 0, 2, '2021-05-12 19:28:57'),
-(55, 0, 3, '2021-05-12 19:28:57'),
-(56, 2, 1, '2021-05-12 19:29:22'),
-(57, 2, 2, '2021-05-12 19:29:22');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=93 ;
 
 -- --------------------------------------------------------
 
@@ -220,16 +215,34 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `id` (`id`),
   KEY `id_2` (`id`),
   KEY `id_3` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `is_admin`, `name`, `class_id`, `take_count`, `wait_count`) VALUES
-(1, 'werf', 'qq', 0, 'Алексей Васютко', 8, 0, 0),
 (2, 'admin', 'admin', 1, 'admin', 3, 0, 0),
-(3, 'qwerty', 'qq', 0, 'qwerty', 5, 0, 0);
+(5, 'one', 'one', 0, 'one one', 5, 0, 0),
+(6, 'two', 'two', 0, 'two two', 8, 0, 0),
+(7, 'three', 'three', 0, 'three three', 1, 0, 0),
+(8, 'four', 'four', 0, 'four four', 19, 0, 0),
+(9, 'five', 'five', 0, 'five five', 19, 0, 0),
+(10, 'six', 'six', 0, 'six six', 17, 0, 0),
+(11, 'seven', 'seven', 0, 'seven seven', 17, 0, 0),
+(12, 'eight', 'eight', 0, 'eight eight', 19, 0, 0),
+(13, 'nine', 'nine', 0, 'nine nine', 16, 0, 0),
+(14, 'ten', 'ten', 0, 'ten ten', 16, 0, 0),
+(15, 'eleven', 'eleven', 0, 'eleven eleven', 1, 0, 0),
+(16, 'twelve', 'twelve', 0, 'twelve twelve', 16, 0, 0),
+(17, 'thirty', 'thirty', 0, 'thirty thirty', 17, 0, 0),
+(18, 'fourty', 'fourty', 0, 'fourty fourty', 14, 0, 0),
+(19, 'fifty', 'fifty', 0, 'fifty fifty', 1, 0, 0),
+(20, 'sixty', 'sixty', 0, 'sixty sixty', 1, 0, 0),
+(21, 'seventy', 'seventy', 0, 'seventy seventy', 10, 0, 0),
+(22, 'eighty', 'eighty', 0, 'eighty', 1, 0, 0),
+(23, 'ninty', 'ninty', 0, 'ninty ninty', 15, 0, 0),
+(24, 'twenty', 'twenty', 0, 'twenty twenty', 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -243,17 +256,7 @@ CREATE TABLE IF NOT EXISTS `waited_books` (
   `id_book` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
-
---
--- Дамп данных таблицы `waited_books`
---
-
-INSERT INTO `waited_books` (`id`, `id_user`, `id_book`, `date`) VALUES
-(11, 1, 6, '2021-05-06 22:19:33'),
-(13, 1, 7, '2021-05-06 22:32:41'),
-(15, 0, 1, '2021-05-07 19:59:55'),
-(16, 1, 4, '2021-05-12 19:21:41');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
